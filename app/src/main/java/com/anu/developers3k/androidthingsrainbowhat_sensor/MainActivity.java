2bd43080bfd88602556b15fd7b870e6afa14769c
Anuraj <anuraj.r@sap.com>
2017-12-27 23:05:38 +0530

    updated the code

--- app/src/main/java/com/anu/developers3k/androidthingsrainbowhat_sensor/MainActivity.java
+++ app/src/main/java/com/anu/developers3k/androidthingsrainbowhat_sensor/MainActivity.java
@@ -11,38 +11,29 @@ import android.os.Handler;
 import com.google.android.things.contrib.driver.bmx280.Bmx280;
 import com.google.android.things.contrib.driver.ht16k33.AlphanumericDisplay;
 import com.google.android.things.contrib.driver.rainbowhat.RainbowHat;
-import com.google.android.things.pio.Gpio;
 
 import java.io.IOException;
 
 /**
- * Use the sesnors in RainbowHAT
+ * Use the sensors in RainbowHAT
  */
 public class MainActivity extends Activity {
 
     Handler handler;
-    Gpio led;
-    boolean value;
     Bmx280 sensor;
     AlphanumericDisplay display;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         try {
             sensor = RainbowHat.openSensor();
             display = RainbowHat.openDisplay();
 
-
-            //choose the green led in the board.
-            led = RainbowHat.openLedRed();
-            led.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
-            handler = new Handler(getMainLooper());
-            value =true;
-
-            //loop for blinking the led
+            sensor.setMode(Bmx280.MODE_NORMAL);
+            sensor.setTemperatureOversampling(Bmx280.OVERSAMPLING_1X);
+           // sensor.setPressureOversampling(Bmx280.OVERSAMPLING_1X);
+            display.setEnabled(true);
             runAndSchedule();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -50,8 +41,9 @@ public class MainActivity extends Activity {
 
     private void runAndSchedule() {
         try {
-            led.setValue(value);
-            value =!value;
+            //displaying the temperature or pressure value in display
+            //display.display(sensor.readPressure());
+            display.display(sensor.readTemperature());
         } catch (IOException e) {
             e.printStackTrace();
         }
